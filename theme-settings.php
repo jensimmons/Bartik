175a75e0f7e23e4a3e6f5c4fa821d4b02f018253
John Albin Wilkins <jwilkins@hedwig.albin.net>
2010-02-03 05:09:04 +0800

    Added list of links sample content for footer regions.

--- theme-settings.php
+++ theme-settings.php
@@ -39,9 +39,33 @@ function bartik_form_system_theme_settings_alter(&$form, &$form_state) {
  */
 function _bartik_process_page(&$variables) {
   // Lorem ipsum is Latin and doesn't need to be translated.
-  $content = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>';
+  $paragraph = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>';
+  // List of links to be used in footer regions.
+  $list = theme('links', array(
+    'links' => array(
+      'jean-bartik' => array(
+        'title' => t('Jean Bartik'),
+        'href' => 'http://www.google.com/search?q=Jean+Bartik',
+        'absolute'  => TRUE,
+      ),
+      'bartik-settings' => array(
+        'title' => t('Bartik theme settings'),
+        'href' => 'admin/appearance/settings/bartik',
+      ),
+      'drupal' => array(
+        'title' => t('Drupal.org'),
+        'href' => 'http://drupal.org',
+        'absolute'  => TRUE,
+      ),
+      'theme-guide' => array(
+        'title' => t('Drupal theme guide'),
+        'href' => 'http://drupal.org/theme-guide',
+        'absolute'  => TRUE,
+      ),
+    ),
+  ));
   // Generate a link to the settings.
-  $content .= '<div class="sample-regions-link">' . t('This sample content can be turned off using the “Display sample content in empty regions” option on the !link page.', array('!link' => l(t('Bartik settings'), 'admin/appearance/settings/bartik'))) . '</div>';
+  $link = '<p class="sample-regions-link">' . t('This sample content can be turned off using the “Display sample content in empty regions” option on the !link page.', array('!link' => l(t('Bartik settings'), 'admin/appearance/settings/bartik'))) . '</p>';
 
   // Create a fake block.
   $block = new stdClass();
@@ -70,6 +94,13 @@ function _bartik_process_page(&$variables) {
       $block->bid    = $bid;
       $block->delta  = 'delta-' . $bid;
       $block->region = $region;
+      // Show the list of links in the footer regions, otherwise show lorem ipsum.
+      if (strpos($region, 'footer_') === 0) {
+        $content = $list . $link;
+      }
+      else {
+        $content = $paragraph . $link;
+      }
       // Load the region.
       $variables['page'][$region] = array(
         'bartik_sample_block' => array(
